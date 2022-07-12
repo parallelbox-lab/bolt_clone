79713315589f6f803f4edecc5fcd7714b715d8c1
Owoeye precious <72979479+parallelbox-lab@users.noreply.github.com>
2022-07-12 22:36:57 +0100

    c

--- lib/screens/mainscreen.dart
+++ lib/screens/mainscreen.dart
@@ -169,14 +169,14 @@ class _MainScreenState extends State<MainScreen> {
             onMapCreated: (controller) {
               _googleMapController = controller;
               setState(() {
-                bottomPaddingOfMap = Platform.isAndroid ? 240 : 270;
+                bottomPaddingOfMap = Platform.isAndroid ? 330 : 340;
                 locatePosition();
               });
             },
           ),
           Positioned(
             right: 10,
-            bottom: Platform.isAndroid ? 240 : 270,
+            bottom: Platform.isAndroid ? 330 : 340,
             // alignment: Alignment.topRight,
             child: GestureDetector(
               onTap: () {
@@ -232,6 +232,136 @@ class _MainScreenState extends State<MainScreen> {
                   ),
                 ),
               )),
+                /* draggable scrollable sheet*/
+            Positioned.fill(
+              child: NotificationListener<DraggableScrollableNotification>(
+                onNotification: (notification) {
+                  setState(() {
+                    percent = 2 * notification.extent - 0.8;
+                  });
+                  return true;
+                },
+                child: DraggableScrollableSheet(
+                  maxChildSize: 0.9,
+                  minChildSize: 0.3,
+                  builder: (BuildContext context, ScrollController scrollController) {
+                    return Material(
+                      elevation: 10.0,
+                      borderRadius: const BorderRadius.vertical(
+                        top: Radius.circular(20.0),
+                      ),
+                      color: Colors.white,
+                      child: Padding(
+                        padding: const EdgeInsets.only(
+                          left: 10.0,
+                          right: 10.0,
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const SizedBox(height: 15.0),
+                            Container(
+                              height: 10.0,
+                              margin: const EdgeInsets.symmetric(horizontal: 120.0),
+                              decoration: BoxDecoration(
+                                color: Colors.grey[200],
+                                borderRadius: BorderRadius.circular(50.0),
+                              ),
+                            ),
+                            const SizedBox(height: 15.0),
+                            Container(
+                              margin: const EdgeInsets.symmetric(horizontal: 5.0),
+                              child: const Text(
+                                "Akwaaba !",
+                                style: TextStyle(
+                                  color: Colors.black54,
+                                ),
+                              ),
+                            ),
+                            Container(
+                              margin: const EdgeInsets.symmetric(
+                                horizontal: 5.0,
+                                vertical: 5.0,
+                              ),
+                              decoration: const BoxDecoration(
+                                color: Colors.white,
+                              ),
+                              child: const Text(
+                                "Where are you going?",
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.w900,
+                                  fontSize: 22.0,
+                                ),
+                              ),
+                            ),
+                            !isDragged
+                                ? GestureDetector(
+                                    onTap: () {
+                                      setState(() {
+                                        percent = 1.0;
+                                      });
+                                    },
+                                    child: TextFormField(
+                                      decoration: InputDecoration(
+                                        enabled: false,
+                                        hintText: "Search Destination",
+                                        filled: true,
+                                        fillColor: Colors.white,
+                                        border: const OutlineInputBorder(
+                                          borderRadius: BorderRadius.all(
+                                            Radius.circular(15.0),
+                                          ),
+                                          gapPadding: 2.0,
+                                        ),
+                                        prefixIcon: Icon(
+                                          Icons.search,
+                                          color: Colors.purple[300],
+                                        ),
+                                      ),
+                                    ),
+                                  )
+                                : Container(),
+                            Expanded(
+                              child: ListView.builder(
+                                controller: scrollController,
+                                padding: const EdgeInsets.only(bottom: 40.0),
+                                itemCount: 20,
+                                itemBuilder: (context, index) {
+                                  return const ListTile(
+                                    contentPadding: EdgeInsets.zero,
+                                    leading: Icon(
+                                      Icons.location_on,
+                                      color: Colors.black,
+                                    ),
+                                    title: Text(
+                                      "Street No 12345 NY Street",
+                                      style: TextStyle(
+                                        color: Colors.black87,
+                                        fontWeight: FontWeight.w700,
+                                      ),
+                                    ),
+                                    subtitle: Text(
+                                      "New York City",
+                                      style: TextStyle(
+                                        color: Colors.black54,
+                                      ),
+                                    ),
+                                  );
+                                },
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ),
+
+
         ]),
       ),
     );
