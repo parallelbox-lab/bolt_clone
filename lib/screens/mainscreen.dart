573076fff1decf6f21d5af85fc0a2c66dbd87b19
Owoeye precious <72979479+parallelbox-lab@users.noreply.github.com>
2022-07-12 20:51:50 +0100

    c

--- lib/screens/mainscreen.dart
+++ lib/screens/mainscreen.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:sizer/sizer.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({ Key? key }) : super(key: key);
@@ -56,6 +57,80 @@ StreamSubscription<Position>? _positionStream;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Column(
+          children: <Widget>[
+            UserAccountsDrawerHeader(
+              decoration: const BoxDecoration(color: Colors.white),
+              accountName: Text('Owoeye Precious',
+                  style: TextStyle(
+                      fontFamily: 'Core Pro',
+                      fontSize: 14.0.sp,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.black)),
+              accountEmail: Text('Edit Profile',
+                  style: TextStyle(
+                      fontFamily: 'Core Pro',
+                      fontSize: 11.0.sp,
+                      fontWeight: FontWeight.w400,
+                      color: Colors.black)),
+              currentAccountPicture: CircleAvatar(
+                  backgroundColor: Colors.white,
+                  child: Image.asset('assets/images/icons/CircleProfile.png',
+                      width: 40, height: 40)),
+            ),
+            Expanded(
+              child: ListView(
+                shrinkWrap: true,
+                children: <Widget>[
+                  const ListTile(
+                    title:  Text('Payment'),
+                  ),
+                  const  ListTile(
+                    title:  Text('Ride History'),
+                  ),
+                  
+                const ListTile(
+                    title:  Text('Work trips'),
+                  ),
+                  const  ListTile(
+                    title:  Text('Support'),
+                  ),
+                  const  ListTile(
+                    title:  Text('About'),
+                  ),
+                  
+                  // ListTile(
+                  //     title: const Text('My Sponsorships', style: NavBar._textStyle),
+                  //     horizontalTitleGap: 2.0,
+                  //     leading: Image.asset(
+                  //         'assets/images/icons/bx_bxs-donate-heart.png',
+                  //         color: Colors.grey,
+                  //         width: 25,
+                  //         height: 25),
+                  //     onTap: () {
+                  //       Navigator.pushNamed(context, MySponsorShip.routeName);
+                  //     }),
+                 
+                  // ListTile(
+                  //     title: const Text('Notifications', style: _textStyle),
+                  //     horizontalTitleGap: 2.0,
+                  //     leading: Image.asset(
+                  //         'assets/images/icons/notification.png',
+                  //         width: 25,
+                  //         height: 25),
+                  //     onTap: () {
+                  //       Navigator.pushNamed(context, Notifications.routeName);
+                  //     }),
+                 
+                 
+                  const Divider(height: 50),
+                  
+                 
+                ],
+              ),
+            ),
+          ],
+        ),
       body: Stack(
         children:[
           GoogleMap(
